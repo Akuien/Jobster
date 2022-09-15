dc78f68af9e0c95e9647d31f6f52ec4c9c29f011
Akuien <91293028+Akuien@users.noreply.github.com>
2022-09-15 14:42:26 +0200

    File Directory correction

--- server/app.js
+++ server/app.js
@@ -37,7 +37,7 @@ app.get('/api', function(req, res) {
     res.json({'message': 'Welcome to your DIT342 backend ExpressJS project!'});
 }); 
 
-const routes = require('./Controllers/index');
+const routes = require('../server/Controllers/index');
 app.use('/api' , routes);
 
 // Catch all non-error handler for api (i.e., 404 Not Found)
