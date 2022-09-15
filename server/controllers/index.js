5d71a23ae731b629bcd6eae820a1bd660cb4d4c0
Akuien <91293028+Akuien@users.noreply.github.com>
2022-09-15 14:48:29 +0200

    File directory

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,9 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
 const CompaniesRoutes = require('./companies');
 const freelancersRoutes = require('./freelancers');
 const Job_post_Routes = require('./job_posts');
 const resumesRoutes = require('./resumes')
 
 module.export = {
+    router,
     CompaniesRoutes,
     freelancersRoutes,
     Job_post_Routes,
