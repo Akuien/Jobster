b85c332d827d91153f759f64685f1309d54f23b8
Anton Golubenko <anton.golubenko@gmail.com>
2022-09-19 01:00:11 +0200

    started changing crud commands

--- server/controllers/freelancers.js
+++ server/controllers/freelancers.js
@@ -1,41 +1,79 @@
 const express = require('express');
+require('dotenv').config(); // this imports .env file to get secret variable
 const router = express.Router();
 const user_login = require("../logins/loginUser");
-const FreelancerCommands = require('../controllers/freelancers');
 const FreelancerModel = require('../models/freelancer');
 const JobPostModel = require('../models/job_post');
+const { MongoClient } = require('mongodb');
+const client = new MongoClient(process.env.MONGODB_URI);//initializing mongo client 
+const connected_client = client.connect();
 
-router.post('/freelancers', (request, response, next) => {
+
+router.post('/freelancers', async (request, response) => {
     
-    const freelancer = new FreelancerModel(request.body);
-
-    freelancer.save(function (err) {
-        if (err) { return next(err); }
-        response.status(201);
-        response.json(freelancer);
-    })
+    try {
+        const freelancer = new FreelancerModel(request.body);
+        
+        (await connected_client)
+            .db('WebDevDatabase')
+            .collection('Freelancers')
+            .insertOne(freelancer);
+        
+        const data = await freelancer.save();
+        response.status(200).json(freelancer);
+    }
+    catch (error) {
+        response.status(400).json({ message: error.message });
+    }
 });
 
-router.get('/freelancers', (request, response, next) => {
+router.get('/freelancers', async (request, response) => {
 
-    FreelancerModel.find(function (error, freelancers) {
-        if (error) { return next(error); }
-        response.json({ "freelancers": freelancers });
-    })
+    try {
+        (await connected_client)
+            .db('WebDevDatabase')
+            .collection('Freelancers')
+            .find({})
+            .toArray(function (error, result) {
+                if (error) {
+                    response.send(error);
+                } else {
+                    // response.send(JSON.stringify(result))
+                    response.json(result);
+                }
+            });
+    }
+    catch (error) {
+        response.status(500).json({ message: error.message });
+    }
 });
 
 
+router.get('/freelancers/:id', async (request, response) => {
 
-router.get('/freelancers/:id', function(request, response, next) {
     const id = request.params.id;
+    
 
-    FreelancerModel.findById(id, function (error, freelancer) {
-        if (error) { return next(error); }
-        if (freelancer == null) {
-            return response.status(404).json({ "message": "Freelancer not found" });
-        }
-        response.json(freelancer);
-    });
+    if (id == null) {
+        return response.status(404).json({ "message": "Freelancer not found" });
+    }
+    try {
+        (await connected_client)
+            .db('WebDevDatabase')
+            .collection('Freelancers')
+            .findOne({ "_id": id }, function (error, freelancer) {
+                if (error) {
+                    response.send(error);
+                } else {
+                   
+                    response.json(freelancer);
+                }
+            });// returns null for now
+        
+    }
+    catch (error) {
+        response.status(500).json({ message: error.message });
+    }
 });
 
 router.patch('/freelancers/:id', function (request, response, next) {
@@ -117,41 +155,41 @@ router.get('/freelancers/:id/job_posts/:id', function (request, response, next) 
             return response.status(404).json({ "message": "Freelancer not found" });
         } 
         
-const loginUser = async (request, response, next, email, password) => {
-    const user = loginUser.user_login(email, password)
+// const loginUser = async (request, response, next, email, password) => {
+//     const user = loginUser.user_login(email, password)
 
-    if (user == "err") {
-        return next(user)
-    }
+//     if (user == "err") {
+//         return next(user)
+//     }
 
-    if (freelancer == "user_not_found") {
-        return response.status(404).json({ "message": "Invalid email", "logged_in": false, })
-    }
+//     if (freelancer == "user_not_found") {
+//         return response.status(404).json({ "message": "Invalid email", "logged_in": false, })
+//     }
 
-    if (freelancer == "invalid_password") {
-        return response.status(401).json({ "message": "Wrong password", "logged_in": false, })
-    }
+//     if (freelancer == "invalid_password") {
+//         return response.status(401).json({ "message": "Wrong password", "logged_in": false, })
+//     }
 
-    response.json({"user": user, "logged_in": true,})
-}
+//     response.json({"user": user, "logged_in": true,})
+// }
 
-const login_user = async (request, response, next, email, password) => {
-    const user = loginUser.loginUser(email, password)
+// const login_user = async (request, response, next, email, password) => {
+//     const user = loginUser.loginUser(email, password)
 
-    if (user == "err") {
-        return next(user)
-    }
+//     if (user == "err") {
+//         return next(user)
+//     }
 
-    if (freelancer == "user_not_found") {
-        return response.status(404).json({ "message": "Invalid email", "logged_in": false, })
-    }
+//     if (freelancer == "user_not_found") {
+//         return response.status(404).json({ "message": "Invalid email", "logged_in": false, })
+//     }
 
-    if (freelancer == "invalid_password") {
-        return response.status(401).json({ "message": "Wrong password", "logged_in": false, })
-    }
+//     if (freelancer == "invalid_password") {
+//         return response.status(401).json({ "message": "Wrong password", "logged_in": false, })
+//     }
 
-    response.json({"user": user, "logged_in": true,})
-}
+//     response.json({"user": user, "logged_in": true,})
+// }
 
 
         JobPostModel.find(function (error, job_post) {
