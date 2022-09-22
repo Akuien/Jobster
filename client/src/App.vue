c15190247d0a53734efdfbaad4f2508373029d13
dovhun@student.chalmers.se <dovhun@student.chalmers.se>
2022-09-22 15:35:44 +0200

    Created a login page

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,30 +3,22 @@
     <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
       <div class="container-fluid">
         <router-link to="/" class="navbar-brand">Jobster</router-link>
-
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="collapsibleNavbar">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <router-link class="nav-link" href="/jobs">Jobs</router-link>
-            </li>
-
-            <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Log in</a>
-              <ul class="dropdown-menu">
-                <li><router-link class="dropdown-item" href="/login">Login</router-link></li>
-                <li><router-link class="dropdown-item" href="/register">Register</router-link></li>
-              </ul>
-            </li>
-          </ul>
-        </div>
+        <ul class="navbar-nav">
+          <li class="nav-item" style="float: left">
+            <router-link class="nav-link" to="/jobs">Jobs</router-link>
+          </li>
+        </ul>
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/login">Login</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/register">Register</router-link>
+          </li>
+        </ul>
       </div>
     </nav>
     <div id="nav">
-      <router-link to="/login"> Log in</router-link>
     </div>
     <!-- Render the content of the current page view -->
     <router-view/>
---
+++ client/src/views/Login.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="form">
+    <form action="/">
+      <h1>Log in</h1>
+      <div class="form-group">
+        <input type="email" class="form-control" placeholder="E-mail">
+      </div>
+
+      <div class="form-group">
+        <input type="password" class="form-control" placeholder="Password">
+      </div>
+    </form>
+  </div>
+</template>
+
+<style>
+  .form {
+    margin: auto;
+    padding: 10px;
+    width: 50%;
+  }
+
+  form {
+    margin-top: 2em;
+  }
+</style>
