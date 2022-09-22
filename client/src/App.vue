e556b350d2bbdbae9a57d5f5529bdfbb06867c3f
dovhun@student.chalmers.se <dovhun@student.chalmers.se>
2022-09-22 14:58:40 +0200

    adding .env.to .gitinore

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,32 @@
 <template>
   <div id="app">
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+      <div class="container-fluid">
+        <router-link to="/" class="navbar-brand">Jobster</router-link>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="collapsibleNavbar">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <router-link class="nav-link" href="/jobs">Jobs</router-link>
+            </li>
+
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">Log in</a>
+              <ul class="dropdown-menu">
+                <li><router-link class="dropdown-item" href="/login">Login</router-link></li>
+                <li><router-link class="dropdown-item" href="/register">Register</router-link></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
     <div id="nav">
-      <router-link to="/">Home</router-link>
+      <router-link to="/login"> Log in</router-link>
     </div>
     <!-- Render the content of the current page view -->
     <router-view/>
@@ -9,6 +34,9 @@
 </template>
 
 <style>
+router-link {
+  color: white;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
