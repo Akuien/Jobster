a1fd5432f3adfe69193ba95140f635793fa193c5
Akuien <91293028+Akuien@users.noreply.github.com>
2022-10-06 16:49:54 +0200

    added get all jobs

--- client/src/views/Jobs.vue
+++ client/src/views/Jobs.vue
@@ -1,25 +1,38 @@
 <template>
-    <div>
-        <p>This should display all the jobs posted </p>
-    </div>
+   <b-row>
+        <div v-for="job_post in job_posts" v-bind:key="job_post._id">
+            <b-card style="height: 20rem;width: 20rem; background-color: lightgrey; padding: 80px; margin-top: 20px; margin-left: 50px; margin-bottom: 20px; position:relative">
+            <h3>{{job_post.job_title}}</h3>
+                <b-card-text>{{job_post.post_date}}</b-card-text>
+                <b-card-text>{{job_post.deadline}}</b-card-text>
+                <b-button style="background-color: grey;" :href="'/job_posts/' + job_post._id">Read more</b-button>
+            </b-card>
+        </div>
+    </b-row>
 </template>
 
 <script>
 import { Api } from '@/Api'
 
 export default {
-  name: 'Jobs',
+  name: 'job_posts',
 
+  mounted() {
+    this.getJobs()
+  },
   data() {
     return {
-      jobs: ''
+      job_posts: []
     }
   },
   methods: {
-    getJobPosts() {
+    getJobs() {
       Api.get('/job_posts')
         .then(response => {
-          this.jobs = response.data.jobs
+          this.job_posts = response.data
+        })
+        .catch(error => {
+          console.log(error)
         })
     }
   }
