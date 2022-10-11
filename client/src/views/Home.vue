46eac3e2ea04640b0d78a5e4721bf09e96842e06
Akuien <91293028+Akuien@users.noreply.github.com>
2022-10-11 17:59:05 +0200

    Design changes

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -7,63 +7,6 @@
         <p>Jobster is a world leading website for customers to get in touch with freelancers. As a customer you can create a job posting and choose the right person to do it for you. As a freelancer you can curate what jobs will appear on your dashboard, get in touch with your customers and earn money with your talents.</p>
         <router-link to="/register">Create your free profile right now!</router-link>
       </div>
-    <div>
-    <b-carousel
-      id="carousel-1"
-      v-model="slide"
-      :interval="4000"
-      controls
-      indicators
-      background="#ababab"
-      img-width="1024"
-      img-height="480"
-      style="text-shadow: 1px 1px 2px #333;"
-      @sliding-start="onSlideStart"
-      @sliding-end="onSlideEnd"
-    >
-      <!-- Text slides with image -->
-      <b-carousel-slide
-        caption="First slide"
-        text="Nulla vitae elit libero, a pharetra augue mollis interdum."
-        img-src="https://picsum.photos/1024/480/?image=52"
-      ></b-carousel-slide>
-
-      <!-- Slides with custom text -->
-      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54">
-        <h1>Hello world!</h1>
-      </b-carousel-slide>
-
-      <!-- Slides with image only -->
-      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=58"></b-carousel-slide>
-
-      <!-- Slides with img slot -->
-      <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
-      <b-carousel-slide>
-        <template #img>
-          <img
-            class="d-block img-fluid w-100"
-            width="1024"
-            height="480"
-            src="https://picsum.photos/1024/480/?image=55"
-            alt="image slot"
-          >
-        </template>
-      </b-carousel-slide>
-
-      <!-- Slide with blank fluid image to maintain slide aspect ratio -->
-      <b-carousel-slide caption="Blank Image" img-blank img-alt="Blank image">
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse eros felis, tincidunt
-          a tincidunt eget, convallis vel est. Ut pellentesque ut lacus vel interdum.
-        </p>
-      </b-carousel-slide>
-    </b-carousel>
-
-    <p class="mt-4">
-      Slide #: {{ slide }}<br>
-      Sliding: {{ sliding }}
-    </p>
-  </div>
     </b-jumbotron>
     </div>
     <div>
@@ -72,11 +15,14 @@
     img-src="https://picsum.photos/900/250/?image=3"
     img-alt="Card Image"
     text-variant="white"
-    title="Image Overlay"
-    sub-title="Subtitle"
+    title="Quality Assurance"
   >
     <b-card-text>
-      Some quick example text to build on the card and make up the bulk of the card's content.
+      Jobster is your best freelancing website available that guarantees you quality delivery to both you as a company and to your
+      customers. Its a place compose of the best talents and networks availabe in the industry for you as a company.
+      The companies available are the best for your as a freelancer, top 5% best companies globally ready to work with you
+      at Jobster with flexible working hours and good pay for your talent. You also get to work with a team of like minded individuals
+      that ensure your personal and career growth.
     </b-card-text>
   </b-card>
 </div>
