6d2e7310f7b9b85123e61c4d75058389b6989e75
Akuien <91293028+Akuien@users.noreply.github.com>
2022-10-14 02:02:35 +0200

    job.vue and freelancers details design improvement

--- client/src/views/Jobs.vue
+++ client/src/views/Jobs.vue
@@ -13,11 +13,16 @@
   <b-button id="delete_jobs_button" @click="deleteJobs()">Delete Jobs</b-button>
    <b-row>
         <div v-for="job_post in job_posts" v-bind:key="job_post._id">
-            <b-card style="height: 20rem;width: 20rem; background-color: lightgrey; padding: 80px; margin-top: 20px; margin-left: 50px; margin-bottom: 20px; position:relative">
-            <h3>{{job_post.job_title}}</h3>
-                <b-card-text>{{job_post.post_date}}</b-card-text>
-                <b-button style="background-color: grey;" :href="'/job_posts/' + job_post._id">Read more</b-button>
-            </b-card>
+          <b-col fluid="md">
+            <div class="card">
+              <img src="https://visaguideinfo.com/wp-content/uploads/2020/02/0.jpg" class="card-img-top" alt="company_pic">
+              <div class="card-body">
+                <h2 class="card-title">{{job_post.job_title}}</h2>
+                <p class="card-text">{{job_post.post_date}}</p>
+                <b-button class="read_more" style="background-color: grey;" :href="'/job_posts/' + job_post._id">Read more</b-button>
+              </div>
+            </div>
+          </b-col>
         </div>
     </b-row>
 </div>
@@ -80,5 +85,46 @@ export default {
 </script>
 
 <style scoped>
+.read_more {
+  background-color: rgb(125, 92, 175);
+}
+  img {
+    height: 12rem;
+    width: 100%;
+  }
+  .card {
+    height: 23rem;
+    width: 20rem;
+    background-color: #d5d7e0;
+    color: rgb(75, 71, 71);
+    margin-top: 40px;
+    margin-left: 50px;
+    margin-bottom: 20px;
+    border-radius: 8px;
+    box-shadow: 0px 8px 15px -7px gray;
+    position: relative;
+    text-align: center;
+    transition: ease-in-out 0.35s;
+}
 
+.card_text {
+  font-size: 15px;
+  line-height: 21px;
+  color: #4D648D;
+  margin-top: 20px;
+}
+
+ .card:hover {
+     transform: scale(1.10);
+     box-shadow: 10px 10px 15px -7px gray;
+  }
+
+.read_more:hover {
+    background-color: rgb(123, 77, 192);
+}
+ @media screen and (max-width: 768px) {
+   .card {
+     margin-left: 10%;
+    }
+ }
 </style>
